438a30115fa6a91f952c5a6ce44beee30b5a7484
qzy <weelgod@gmail.com>
2018-06-25 19:45:39 +0800

    init

---
+++ src/button.vue
@@ -0,0 +1,53 @@
+<template>
+    <button class="g-button">按钮</button>
+</template>
+
+<script>
+    export default {
+        name: "button"
+    }
+</script>
+
+<style scoped lang='scss'>
+    .g-button {
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+        &:focus {
+            outline: none;
+        }
+        > .icon {
+            order: 1;
+            margin-right: .3em;
+        }
+        > .content {
+            order: 2;
+        }
+        &.icon-right {
+            > .icon {
+                order: 2;
+                margin-left: .3em;
+                margin-right: 0;
+            }
+            > .content {
+                order: 1;
+            }
+        }
+        .loading {
+            animation: spin 2s infinite linear;
+        }
+    }
+</style>
