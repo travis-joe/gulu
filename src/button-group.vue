f158c3440c348470e2bdce4a1fc74df646b1c92f
wb-qcy313750 <wb-qcy313750@antfin.com>
2018-06-26 10:30:13 +0800

    stylebug

--- src/button-group.vue
+++ src/button-group.vue
@@ -7,32 +7,34 @@
 <script>
   export default {
     mounted() {
-      for( let node of this.$el.children) {
-        let name = node.nodeName.toLowerCase()
-        if(name !== 'button') {
-          console.warn('g-button-group的子元素应该是button,但你写了'+ name)
+      for (let node of this.$el.children) {
+        let name = node.nodeName.toLowerCase();
+        if (name !== "button") {
+          console.warn("g-button-group的子元素应该是button,但你写了" + name);
         }
       }
     }
-  }
+  };
 </script>
 
 <style scoped lang="scss">
-    .g-button-group{
+    .g-button-group {
         display: inline-flex;
         vertical-align: middle;
         > .g-button {
             border-radius: 0;
-            margin-left: -1px;
-            &:first-child{
+            &:not(:first-child) {
+                margin-left: -1px;
+            }
+            &:first-child {
                 border-top-left-radius: var(--border-radius);
                 border-bottom-left-radius: var(--border-radius);
             }
-            &:last-child{
+            &:last-child {
                 border-top-right-radius: var(--border-radius);
                 border-bottom-right-radius: var(--border-radius);
             }
-            &:hover{
+            &:hover {
                 position: relative;
                 z-index: 1;
             }
