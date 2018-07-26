d8fac2f414368d4caa508452337d8f312c19a529
wb-qcy313750 <wb-qcy313750@antfin.com>
2018-07-26 10:35:08 +0800

    tabs测试

--- test/tabs.test.js
+++ test/tabs.test.js
@@ -9,7 +9,7 @@ Vue.config.productionTip = false
 Vue.config.devtools = false
 Vue.component('g-tabs', Tabs)
 Vue.component('g-tabs-head', TabsHead)
-Vue.component('g-tabs-head', TabsBody)
+Vue.component('g-tabs-body', TabsBody)
 Vue.component('g-tabs-item', TabsItem)
 Vue.component('g-tabs-pane', TabsPane)
 
@@ -18,13 +18,31 @@ describe('Tabs', () => {
   it('存在.', () => {
     expect(Tabs).to.exist
   })
-  it('接受selected属性', () => {
+  it('子组件只能是tabs-head和tabs-body', (done) => {
     const div = document.createElement('div');
     document.body.appendChild(div);
     div.innerHTML = `
-      <g-tabs>
-        <div>hi</div>
+      <g-tabs selected="finance">
+          <g-tabs-head>
+              <template slot="actions">设置</template>
+              <g-tabs-item name="beauty" disabled>美女</g-tabs-item>
+              <g-tabs-item name="finance">财经</g-tabs-item>
+              <g-tabs-item name="sports">体育</g-tabs-item>
+          </g-tabs-head>
+          <g-tabs-body>
+              <g-tabs-pane name="beauty">美女相关内容</g-tabs-pane>
+              <g-tabs-pane name="finance">财经相关内容</g-tabs-pane>
+              <g-tabs-pane name="sports">体育相关内容</g-tabs-pane>
+          </g-tabs-body>
       </g-tabs>
       `
+    let vm = new Vue({
+      el:div
+    })
+    setTimeout(() => {
+      let x = vm.$el.querySelector('.tabs-item[data-name="finace"]');
+      expect(x.classList.contains('active')).to.be.true;
+      done();
+    }, 1000);
   })
 })
