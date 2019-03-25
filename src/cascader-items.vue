6791b0d459a60047cbf0879942a5514254c18812
wb-qcy313750 <wb-qcy313750@antfin.com>
2019-03-25 14:56:33 +0800

    parcel 还好不慢

--- src/cascader-items.vue
+++ src/cascader-items.vue
@@ -1,30 +1,39 @@
 <template>
-    <div class="cascaderItems">
+    <div class="cascaderItems" :style="{height:height}">
         <div class="left">
-            <div class="label" v-for="item in items" @click="leftSelected = item">{{item.name}}</div>
+            <div class="label" v-for="item in items" @click="leftSelected = item">
+                {{item.name}}
+                <icon name="right" v-if="item.children" class="icon"></icon>
+            </div>
         </div>
         <div class="right" v-if="rightItems">
-            <Cascader-Items :items="rightItems"></Cascader-Items>
+            <Cascader-Items :items="rightItems" :height="height"></Cascader-Items>
         </div>
     </div>
 </template>
 
 <script>
+  import Icon from './icon'
+
   export default {
     name: "CascaderItems",
+    components: {Icon},
     props: {
       items: {
         type: Array
+      },
+      height: {
+        type: String
       }
     },
     data() {
-        return {
-          leftSelected: null
-        }
+      return {
+        leftSelected: null
+      }
     },
     computed: {
       rightItems() {
-        if(this.leftSelected && this.leftSelected.children) {
+        if (this.leftSelected && this.leftSelected.children) {
           return this.leftSelected.children
         } else {
           return null
@@ -36,12 +45,32 @@
 </script>
 
 <style scoped lang="scss">
+    @import "var";
+
     .cascaderItems {
         display: flex;
-        .left{
-            border: 1px solid red;
+        height: 100px;
+
+        .left {
+            padding: .3em 0;
+
+            .label {
+                padding: .3em 1em;
+                cursor: pointer;
+                white-space: nowrap;
+                display: flex;
+                align-items: center;
+                .icon {
+                    margin-left: 1em;
+                    transform: scale(.5);
+                    opacity: 0.5;
+                }
+            }
         }
-        .right{
+
+        .right {
+            border-left: 1px solid lighten($border-color, 35%);
         }
+
     }
 </style>
--- src/cascader.vue
+++ src/cascader.vue
@@ -3,8 +3,8 @@
         <div class="trigger" @click="popoverVisible = !popoverVisible">
             <slot></slot>
         </div>
-        <div class="popover" v-if="popoverVisible">
-            <cascader-items :items="source"></cascader-items>
+        <div class="popover-wrapper" v-if="popoverVisible">
+            <cascader-items :items="source" class="popover" :height="popoverHeight"></cascader-items>
         </div>
     </div>
 </template>
@@ -18,6 +18,9 @@
     props: {
       source: {
         type: Array
+      },
+      popoverHeight: {
+        type: String
       }
     },
     data() {
@@ -33,15 +36,17 @@
     @import "var";
 
     .cascader {
-        height: $input-height;
+        position: relative;
         .trigger{
-            border: 1px solid $red;
+            border: 1px solid black;
             height: $input-height;
             width: 100px;
         }
-        .popover{
-            border: 1px solid $red;
-            height: 200px;
+        .popover-wrapper{
+            position: absolute;
+            top:100%;
+            left:0;
+            @extend .box-shadow;
         }
     }
 </style>
